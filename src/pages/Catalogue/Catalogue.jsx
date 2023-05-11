a3c985709727c427bc1b300947dc53b10c44c6f9
Darika3 <darika.zholborsova@gmail.com>
2023-05-11 17:11:45 +0600

    adaptive

--- src/pages/Catalogue/Catalogue.jsx
+++ src/pages/Catalogue/Catalogue.jsx
@@ -3,24 +3,48 @@ import eye from "../assets/eye.png";
 import "../Catalogue/Catalogue.css";
 import lip from "../assets/lip.png";
 import face from "../assets/face.png";
+import Navbar from "../../components/Navbar/Navbar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import faceBg from "../assets/faceBg.jpg";
+import eyebg from "../assets/eyeBg.jpg";
+import lipsBg from "../assets/lipsBg.jpg";
 const Catalogue = () => {
   return (
-    <div id="catalogueContainer">
-      <h2>SHOP BY CATEGORY</h2>
-      <div id="catalogueWhiteBg">
-        <div id="categoryLeft" className="category">
-          <img className="categoryTitle" src={eye} alt="" />
-        </div>
-        <div id="categoryRight">
-          <div id="lipCont" className="category">
-            <img className="categoryTitle" src={lip} alt="" />
+    <>
+      <Navbar />
+      <div id="catalogueContainer">
+        <h2>SHOP BY CATEGORY</h2>
+        <div id="catalogueWhiteBg">
+          <div id="categoryLeft" className="categoryEye">
+            <img className="categoryTitle" src={eye} alt="" />
           </div>
-          <div id="faceCont" className="category">
-            <img className="categoryTitle" src={face} alt="" />
+          <div id="categoryRight">
+            <div id="lipCont" className="categoryLip">
+              <img className="categoryTitle" src={lip} alt="" />
+            </div>
+            <div id="faceCont" className="categoryFace">
+              <img className="categoryTitle" src={face} alt="" />
+            </div>
+          </div>
+        </div>
+        <div id="adaptcatalogue">
+          <div className="picEye">
+            <img className="title" src={eye} alt="" />
+          </div>
+
+          <div className="picLip">
+            <img className="title" src={lip} alt="" />
+          </div>
+          <div className="picFace">
+            <img className="title" src={face} alt="" />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import HomeBg from "../assets/homeBg.png";
 import "../HomePage/Home.css";
+import ProductList from "../../components/Product/ProductList/ProductList";
 const Home = () => {
   return (
     <div>
@@ -10,10 +11,13 @@ const Home = () => {
         <div id="leftHomePage">
           <h1>YOUR FAVE LIP COMBO</h1>
           <h3>shop your favorite lip kits</h3>
-          <button>SHOP NOW</button>
+          <a href="#all">
+            <button>SHOP NOW</button>
+          </a>
         </div>
         <img id="rightHomePage" src={HomeBg} alt="" />
       </div>
+      <ProductList />
     </div>
   );
 };
