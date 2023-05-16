ecc44f9ea9551ae48ca4e008438e340c26f55716
Darika3 <darika.zholborsova@gmail.com>
2023-05-16 16:31:30 +0600

    merged

--- src/components/Product/AddProductSkin.jsx
+++ src/components/Product/AddProductSkin.jsx
@@ -1,26 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./AddProduct.css";
 
 import NavbarSkin from "../Navbar/NavbarSkin";
 import FooterSkin from "../Footer/FooterSkin";
+import { useProducts } from "../../contexts/ProductContextProvider";
 
 const AddProductSkin = () => {
+  const { addProduct } = useProducts();
+  const [product, setProduct] = useState({
+    title: "",
+    image: "",
+    type: "",
+    description: "",
+    price: "",
+    category: "",
+  });
+  const handleInp = (e) => {
+    if (e.target.name === "price") {
+      let obj = {
+        ...product,
+        [e.target.name]: Number(e.target.value),
+      };
+      setProduct(obj);
+    } else if (e.target.value.trim() === "") {
+      e.preventDefault();
+      alert("Please, fill in the fields!");
+      addProduct((e) => e.preventDefault());
+    } else {
+      let obj = {
+        ...product,
+        [e.target.name]: e.target.value,
+      };
+      setProduct(obj);
+    }
+  };
+  console.log(product);
   return (
     <>
       <NavbarSkin />
       <div id="addProductSkin">
         <div className="inpContainerAdd">
-          <input className="inpAddProduct" type="text" placeholder="Title" />
-          <input className="inpAddProduct" type="url" placeholder="Image" />
-          <input className="inpAddProduct" type="text" placeholder="Type" />
           <input
+            onChange={handleInp}
             className="inpAddProduct"
             type="text"
-            placeholder="Description"
+            name="title"
+            placeholder="Title"
+            required
           />
-          <input className="inpAddProduct" type="number" placeholder="Price" />
-          <button className="btnAddProductSkin">ADD PRODUCT</button>
+          <input
+            onChange={handleInp}
+            className="inpAddProduct"
+            type="url"
+            name="image"
+            placeholder="Image"
+            required
+          />
+          <input
+            onChange={handleInp}
+            className="inpAddProduct"
+            type="text"
+            name="type"
+            placeholder="Type"
+            required
+          />
+          <input
+            onChange={handleInp}
+            className="inpAddProduct"
+            type="text"
+            name="description"
+            placeholder="Description"
+            required
+          />
+          <input
+            onChange={handleInp}
+            className="inpAddProduct"
+            type="number"
+            name="price"
+            placeholder="Price"
+            required
+          />
+          <button
+            className="btnAddProductSkin"
+            onClick={() => addProduct(product)}
+          >
+            ADD PRODUCT
+          </button>
         </div>
       </div>
       <FooterSkin />
