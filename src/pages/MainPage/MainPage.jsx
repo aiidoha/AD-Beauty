6c0cad4b31c2dad7a2df5bf8592c3906b1a8ab0a
Darika3 <darika.zholborsova@gmail.com>
2023-05-16 21:11:28 +0600

    mainPage animation

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import logo from "../assets/ad_logo.png";
 import decorMain from "../assets/decor.png";
 import skincareMain from "../assets/skincare.png";
@@ -6,24 +6,74 @@ import "../MainPage/Main.css";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import videodecor from "../MainPage/decorMain.mp4";
+import skinMain from "../MainPage/skinMainMob.mp4";
+
 const MainPage = () => {
   const navigate = useNavigate();
+  const decorVideoRef = useRef(null);
+  const skinVideoRef = useRef(null);
+  const [isDecorPlaying, setIsDecorPlaying] = useState(false);
+  const [isSkinPlaying, setIsSkinPlaying] = useState(false);
+
+  const handleDecorMouseEnter = () => {
+    if (decorVideoRef.current) {
+      decorVideoRef.current.play();
+      setIsDecorPlaying(true);
+    }
+  };
+
+  const handleDecorMouseLeave = () => {
+    if (decorVideoRef.current) {
+      decorVideoRef.current.pause();
+      setIsDecorPlaying(false);
+    }
+  };
+
+  const handleSkinMouseEnter = () => {
+    if (skinVideoRef.current) {
+      skinVideoRef.current.play();
+      setIsSkinPlaying(true);
+    }
+  };
+
+  const handleSkinMouseLeave = () => {
+    if (skinVideoRef.current) {
+      skinVideoRef.current.pause();
+      setIsSkinPlaying(false);
+    }
+  };
+
   return (
     <div id="cont" style={{ position: "relative" }}>
       <img id="mainLogo" src={logo} alt="" />
       <div id="MainPics">
-        <img
-          id="decorMain"
-          src={decorMain}
-          alt=""
-          onClick={() => navigate(`/home`)}
-        />
-        <img
-          id="skincareMain"
-          src={skincareMain}
-          alt=""
-          onClick={() => navigate("/homeSkin")}
-        />
+        <div id="decorVid">
+          <video
+            id="decorMain"
+            ref={decorVideoRef}
+            onMouseEnter={handleDecorMouseEnter}
+            onMouseLeave={handleDecorMouseLeave}
+            onClick={() => navigate(`/home`)}
+            muted
+            autoPlay={isDecorPlaying}
+          >
+            <source src={videodecor} type="video/mp4" />
+          </video>
+        </div>
+        <div id="skinVid">
+          <video
+            id="skincareMain"
+            ref={skinVideoRef}
+            onMouseEnter={handleSkinMouseEnter}
+            onMouseLeave={handleSkinMouseLeave}
+            onClick={() => navigate(`/homeSkin`)}
+            muted
+            autoPlay={isSkinPlaying}
+          >
+            <source src={skinMain} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </div>
   );
